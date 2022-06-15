820e73bfdbf242dc085c958e2f68d45dcb129680
Typiespectre <ec2-user@ip-172-26-13-69.ap-northeast-2.compute.internal>
2022-06-15 13:24:55 +0000

    modified: testing some functions in server.py

--- backend/server.py
+++ backend/server.py
@@ -4,9 +4,14 @@
 #  python 3.7.10  #
 ###################
 
+import uvicorn
 from fastapi import FastAPI
+from database import startup
 from controller import get_all_articles
 
+
+APP_HOST_ADDRESS = "0.0.0.0"
+APP_PORT = 8000
 
 app = FastAPI()
 
@@ -14,3 +19,17 @@ app = FastAPI()
 def show_all_articles():
     return {"articles": get_all_articles()}
 
+@app.get("/test/{test_id}")
+def test_get(test_id : int):
+    return {"test_id": test_id}
+
+#def serve():
+#    uvicorn.run(app, host=APP_HOST_ADDRESS, port=APP_PORT)
+
+if __name__=="__main__":
+    startup()
+    uvicorn.run("server:app",
+                host=APP_HOST_ADDRESS,
+                port=APP_PORT,
+                reload=True,
+                )
