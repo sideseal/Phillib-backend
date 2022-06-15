c1192542cff34e472ad65f85ff21afdadaabf207
Typiespectre <ec2-user@ip-172-26-13-69.ap-northeast-2.compute.internal>
2022-06-15 13:27:49 +0000

    modified: modify file to work short and clean

--- backend/scrapper.py
+++ backend/scrapper.py
@@ -1,33 +1,29 @@
 #!/usr/bin/env python3
 
-##################
-#   python 3.7   #
-##################
+###################
+#  python 3.7.10  #
+###################
 
 import datetime
-from database import session
+from database import startup, shutdown
 from models import Article
 from controller import add_articles
 from sites.aeon import aeon_rss
 
+
 def main():
     target_sites = [aeon_rss("https://aeon.co/feed.rss"),]
-    total_articles_count = 0
-    total_status = False
 
-    for site in target_sites:
-        counts, status = add_articles(site)
-        if not status:
-            break
-        total_articles_count += counts
+    total_articles_count, status = add_articles(target_sites)
 
-    total_status = status
-    if total_status:
-        print(f"{datetime.datetime.now()}: *JOB DONE!* Articles found: {total_articles_count}")
+    if status:
+        print(f"{datetime.date.today()}: *JOB DONE!* Articles found: {total_articles_count}")
     else:
         print(f"{datetime.datetime.now()}: *ERROR!*")
 
-    session.close(); return
+    return status
 
 if __name__ == "__main__":
+    startup()
     main()
+    shutdown()
