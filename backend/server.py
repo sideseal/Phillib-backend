da9acb9d0d21b41bd16445b7f91c6cb449f8673c
Typiespectre <ec2-user@ip-172-26-13-69.ap-northeast-2.compute.internal>
2022-07-03 18:24:53 +0000

    modified: delete mkcert certs

--- backend/server.py
+++ backend/server.py
@@ -5,15 +5,27 @@
 ###################
 
 import uvicorn
+import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from database import startup
 from controller import get_all_articles
 
-
+PATH = os.environ["HOME"]
 APP_HOST_ADDRESS = "0.0.0.0"
 APP_PORT = 8000
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def show_all_articles():
@@ -30,8 +42,8 @@ if __name__=="__main__":
     startup()
     uvicorn.run("server:app",
                 host=APP_HOST_ADDRESS,
-                # port=APP_PORT,
-                reload=True,
-                ssl_keyfile="./key.pem",
-                ssl_certfile="./cert.pem",
+                port=APP_PORT,
+                # reload=True,
+                # ssl_keyfile="/etc/letsencrypt/live/api.phillib.com/privkey.pem",
+                # ssl_certfile="/etc/letsencrypt/live/api.phillib.com/fullchain.pem",
                 )
